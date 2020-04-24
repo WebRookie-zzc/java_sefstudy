68ddbb5964b93a5a1ace5b09ebae3a3e2e7c94ca
zhaizhichao1929 <zzc1919687026@163.com>
2020-04-24 10:47:53 +0800

    'sixth'

--- fuben/src/second.java
+++ fuben/src/second.java
@@ -27,18 +27,19 @@ public class second {
         System.out.println(f);
 
         //输入函数
-//        System.out.print("请输入");
-//        Scanner input = new Scanner(System.in);
-//        int day = input.nextInt();
+        System.out.print("请输入");
+        Scanner input = new Scanner(System.in);
+       int day = input.nextInt();
         //nextInt用于读取输入的数字
         //nextLine用于读取字符串
-//        System.out.println(day);
+       System.out.println(day);
 
         //if 分支
-        /*System.out.println("请输入月份：");
+        System.out.println("请输入月份：");
         Scanner input_1 = new Scanner(System.in);
         int month = input_1.nextInt();
         if(month ==3 | month ==4  | month ==5){
+        //写成if(month>=3 && month<=5)更简洁
             System.out.println("春天");
         }else if(month == 6|month ==7|month ==8) {
             System.out.println("夏天");
@@ -48,18 +49,50 @@ public class second {
             System.out.println("冬天");
         }else{
             System.out.println("数据错误");
-        }*/
+        }
 
         //摇骰子
         Random random = new Random();
         int num = random.nextInt(7);
         //System.out.println(num);
-        Scanner input = new Scanner(System.in);
-        int num_in = input.nextInt();
+        Scanner input_3 = new Scanner(System.in);
+        int num_in = input_3.nextInt();
         if(num_in == num){
             System.out.println("猜对了");
         }else{
             System.out.println("猜错了");
+        }
+
+        //判断成绩
+        System.out.println("请输入分数");
+        Scanner input_2 = new Scanner(System.in);
+        int result = input_2.nextInt() / 10;
+        switch (result){
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("不及格");
+                break;
+            case 6:
+                System.out.println("及格");
+                break;
+            case 7:
+                System.out.println("中");
+                break;
+            case 8:
+                System.out.println("良");
+                break;
+            case 9:
+                System.out.println("优秀");
+                break;
+            case 10:
+                System.out.println("满分");
+                break;
+            default:
+                System.out.println("数据错误");
         }
     }
 }
--- note/sixm.md
+++ note/sixm.md
@@ -1,6 +1,6 @@
 # sixth
 
-## Java语法结构1
+## Java语法结构
 
 补充一下按位非 ~：就是将每一位的0换为1，1换为0。
 
@@ -113,4 +113,58 @@ public class{
             System.out.println("猜错了");
     }
 }
+```
+
+
+```java
+import java.lang.Math  //lang的包相当于桌面，这步导入可以省略掉(lang包的都可省略)
+public class second{
+    public static void main(String[] args){
+        //new一个对象也省了，因为里面都是静态方法
+        double value = Math.random();//Math.random()返回的值是[0,1)的double类型
+        int result = (int)(value*6+1);
+        System.out.println("本次摇出的结果是：",result);//可以进行拼接
+    }
+}
+```
+
+
+补充：引用数据类型(如字符串)的“==”要用equals()方法，如果用==的话，那么比较的就是地址
+```"a".equals("b")  --->  flase```
+
+
+### 多分支语句
+
+```switch(值){
+    case值1：
+        语句一
+        break;
+    case值2；
+        语句二
+        break;
+    default:
+        语句三
+        break;
+}```
+
+注意：小括号里面的值是 byte short  int  char enum  1.7String类型
+
+当小括号里面的值和case后面的值相同的时候才会执行case后面的语句
+
+default相当于else
+
+注意：java和C相同，case只是标识作用，需要用break跳出，如果没有break的时候，则从满足条件的开始后面的所有的case后的代码都执行(break不是必须的)
+
+if和switch：
+if的小括号里可以写复杂的逻辑，但是执行较慢
+switch判断的过程效率更高,但只能做“==”判断
+
+练习：用switch表示范围：
+
+题目：利用谁提出语句实现一个学生成绩对应的区间
+不及格 <60   及格【60,70)  中【70,80)   良【80,90） 优秀【90,100）  满分100  数据有误
+
+
+```java
+
 ```
